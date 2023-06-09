acd482a68fb1169f9dbe48ed50fd15045edf0d66
srkarnik <@NikitaKaravaev25>
2023-06-09 11:06:17 +0300

    132

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, flash, redirect, session, url_for, abort, g
+from flask import Flask, render_template, request, flash, redirect, session, url_for, abort, g, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+from UserLogin import UserLogin
 
 from FDataBase import FDataBase
 
@@ -14,6 +17,14 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print("load_user")
+    return UserLogin().fromDB(user_id, dbase)
 
 
 def connect_db():
@@ -36,12 +47,14 @@ def get_db():
     return g.link_db
 
 
-@app.route("/")
-def index():
+dbase = None
+
+
+@app.before_request
+def before_request():
+    global dbase
     db = get_db()
     dbase = FDataBase(db)
-    return render_template('index.html', menu = dbase.getMenu(), posts=dbase.getPostsAnonce())
-
 
 
 @app.teardown_appcontext
@@ -49,6 +62,11 @@ def close_db(error):
     '''Закрываем соединение с БД, если оно было установлено'''
     if hasattr(g, 'link_db'):
         g.link_db.close()
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
 @app.route("/about")
@@ -66,8 +84,6 @@ def profile(username):
 
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
-    db = get_db()
-    dbase = FDataBase(db)
     if request.method == 'POST':
         if len(request.form['username']) > 2:
             flash('Сообщение отправлено', category='success')
@@ -78,32 +94,58 @@ def contact():
 
 @app.errorhandler(404)
 def pageNotFount(error):
-    db = get_db()
-    dbase = FDataBase(db)
     return render_template('page404.html', title="Страница не найдена", menu=dbase.getMenu()), 404
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    db = get_db()
-    dbase = FDataBase(db)
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == "nick" and request.form['psw'] == "123":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
+    if request.method == "POST":
+        user = dbase.getUserByEmail(request.form['email'])
+        if user and check_password_hash(user['psw'], request.form['psw']):
+            userlogin = UserLogin().create(user)
+            login_user(userlogin)
+            return redirect(url_for('index'))
 
-    return render_template('login.html', title="Авторизация", menu=dbase.getMenu())
+        flash("Неверная пара логин/пароль", "error")
+
+    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация")
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        session.pop('_flashes', None)
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
+                and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['name'], request.form['email'], hash)
+            if res:
+                flash("Вы успешно зарегистрированы", "success")
+                return redirect(url_for('login'))
+            else:
+                flash("Ошибка при добавлении в БД", "error")
+        else:
+            flash("Неверно заполнены поля", "error")
+
+    return render_template("register.html", menu=dbase.getMenu(), title="Регистрация")
+
+@app.route('/profile')
+@login_required
+def profile():
+    return f"""<a href="{url_for('logout')}">Выйти из профиля</a>
+                user info: {current_user.get_id()}"""
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
 
 
 @app.route("/add_post", methods=["POST", "GET"])
 def addPost():
-    db = get_db()
-    dbase = FDataBase(db)
-
     if request.method == "POST":
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
-            res = dbase.addPost(request.form['name'], request.form['post'])
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
             if not res:
                 flash('Ошибка добавления статьи', category='error')
             else:
@@ -113,16 +155,15 @@ def addPost():
 
     return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
 
-
-@app.route("/post/<int:id_post>")
-def showPost(id_post):
-    db = get_db()
-    dbase = FDataBase(db)
-    title, post = dbase.getPost(id_post)
+@login_required
+@app.route("/post/<alias>")
+def showPost(alias):
+    title, post = dbase.getPost(alias)
     if not title:
         abort(404)
 
     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
